b3efcf5efed3864f6cffdd0b246ebdde89132c7e
vijay112bytes <vijaykiran112@gmail.com>
2026-03-22 21:20:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-const weddingDate = new Date("April 25, 2026 18:00:00").getTime();
+// OPEN SCROLL
+function openInvite() {
+    document.getElementById("intro").style.display = "none";
+    document.getElementById("scrollWrapper").classList.remove("hidden");
 
-const timer = setInterval(function () {
-    const now = new Date().getTime();
-    const diff = weddingDate - now;
+    startTyping();
+}
 
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
-    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+// TYPING EFFECT
+const text = "Rahul ❤️ Priya";
+let i = 0;
 
-    document.getElementById("countdown").innerHTML =
-        days + "d " + hours + "h " + minutes + "m left";
+function startTyping() {
+    const el = document.getElementById("names");
+    const interval = setInterval(() => {
+        el.innerHTML += text[i];
+        i++;
+        if (i === text.length) clearInterval(interval);
+    }, 100);
+}
 
-    if (diff < 0) {
-        clearInterval(timer);
-        document.getElementById("countdown").innerHTML = "It's Wedding Time!";
-    }
-}, 1000);
+// MUSIC
+const btn = document.getElementById("musicBtn");
+const music = document.getElementById("bgMusic");
+
+btn.onclick = () => {
+    if (music.paused) music.play();
+    else music.pause();
+};
